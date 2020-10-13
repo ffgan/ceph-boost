5caa84a0302f394b2ff31be580ab454772457b96
Giovanni Mascellani <gio@debian.org>
2020-10-13 10:34:12 +0200

    Update update script to python3.

--- debian/update-control.py
+++ debian/update-control.py
@@ -1,5 +1,4 @@
-#! /usr/bin/env python
-#
+#!/usr/bin/env python3
 
 from deb822 import Deb822
 import re
@@ -38,13 +37,13 @@ def replaceVersion(string, ver1, ver2):
     return string
 
 def updateVersionedValue(paragraph, key):
-    if not paragraph.has_key(key): return
+    if key not in paragraph: return
     oldValue = paragraph[key]
     paragraph[key] = replaceVersion(paragraph[key], gOldVersion, gNewVersion)
     return (oldValue, paragraph[key])
 
 def conflictsWithPrevious(paragraph):
-    if not paragraph.has_key('Conflicts'): return False
+    if 'Conflicts' not in paragraph: return False
     nameRe = re.sub('\d', '\\d', paragraph['Package'])
     return re.search(nameRe, paragraph['Conflicts']) is not None
 
@@ -52,7 +51,7 @@ def updateConflicts(paragraph, oldPkgName):
     newPkgName = paragraph['Package']
     needsConflict = (newPkgName.endswith("-dev") and not newPkgName.endswith("-all-dev")) or conflictsWithPrevious(paragraph)
     if not needsConflict: return
-    if paragraph.has_key('Conflicts'):
+    if 'Conflicts' in paragraph:
         if paragraph['Conflicts'].find(oldPkgName) == -1:
             paragraph['Conflicts'] += ', ' + oldPkgName
     else:
@@ -69,8 +68,8 @@ def processPackageParagraph(p):
     updateConflicts(p, oldPkgName)
 
 def printParagraph(p):
-    for key in p.keys():
-        print "%s: %s" % (key, p[key])
+    for key in list(p.keys()):
+        print("%s: %s" % (key, p[key]))
 
 def processControl():
     firstParagraph = True
@@ -81,7 +80,7 @@ def processControl():
             firstParagraph = False
         else:
             processPackageParagraph(paragraph)
-            print
+            print()
             printParagraph(paragraph)
 
 
